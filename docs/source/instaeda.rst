79f006556102cacdf9577767f09ea394eccf4b69
Justin Fu <jufu11@gmail.com>
2021-03-03 22:42:51 -0800

    add fixture input_dataframe to use for test

---
+++ docs/source/instaeda.rst
@@ -0,0 +1,21 @@
+instaeda package
+================
+
+Submodules
+----------
+
+instaeda.instaeda module
+------------------------
+
+.. automodule:: instaeda.instaeda
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: instaeda
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- tests/test_instaeda.py
+++ tests/test_instaeda.py
@@ -1,5 +1,19 @@
 from instaeda import __version__
 from instaeda import instaeda
+import pytest
+from palmerpenguins import load_penguins
+import pandas as pd
+import altair
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.fixture
+def input_dataframe():    
+    penguin_df = load_penguins()
+    return penguin_df
+
+def test_input_df(input_dataframe):
+    rows_in_df = len(input_dataframe)
+    assert rows_in_df == 344
+    
